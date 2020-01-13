aaa57ae73467f832468cfbc1eba4596e2cbbe80e
AL <al12ts@hotmail.com>
2020-01-13 13:56:29 +0100

    Update translations.

--- src/preview_base_en.ts
+++ src/preview_base_en.ts
@@ -4,8 +4,18 @@
 <context>
     <name>PreviewBase</name>
     <message>
-        <location filename="previewbase.cpp" line="77"/>
+        <location filename="previewbase.cpp" line="91"/>
         <source>Supports previewing various types of data files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="previewbase.cpp" line="107"/>
+        <source>Enable previewing of basic file types, such as images and text files.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="previewbase.cpp" line="108"/>
+        <source>Specify a list of comma separated extensions (without &quot;.&quot;) that should not be previewed by this plugin.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
